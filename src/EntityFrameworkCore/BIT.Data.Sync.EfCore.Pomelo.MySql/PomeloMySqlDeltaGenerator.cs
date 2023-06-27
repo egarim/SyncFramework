47c5bd4fde9395927c709a8d8542c0471e7782ab
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-06-27 23:57:45 +0300

    Fixed bad merge

--- src/EntityFrameworkCore/BIT.Data.Sync.EfCore.Pomelo.MySql/PomeloMySqlDeltaGenerator.cs
+++ src/EntityFrameworkCore/BIT.Data.Sync.EfCore.Pomelo.MySql/PomeloMySqlDeltaGenerator.cs
@@ -51,11 +51,6 @@ namespace BIT.Data.Sync.EfCore.Sqlite
             optionsUpdateSqlGenerator = GetOptions(serverVersion);
             var mySqlUpdateSqlGenerator = new MySqlUpdateSqlGenerator(new UpdateSqlGeneratorDependencies(sqlGenerationHelper, typeMappingSource), optionsUpdateSqlGenerator);
 
-            MySqlSqlGenerationHelper sqlGenerationHelper = new MySqlSqlGenerationHelper(dependencies, optionsTypeMappserverVersion      MySqlTypeMappingSource typeMappingSource = new MySqlTypeMappingSource(TypeMappingSourceDependencies, RelationalTypeMappingSourceDependencies, optionsTypeMapping);
-           
-            var mySqlUpdateSqlGenerator = new MySqlUpdateSqlGenerator(new UpdateSqlGeneratorDependencies(sqlGenerationHelper, typeMappingSource));
-
-
 
 
             return mySqlUpdateSqlGenerator;
