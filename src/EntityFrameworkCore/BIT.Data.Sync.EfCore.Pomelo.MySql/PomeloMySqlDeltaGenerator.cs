00fc4e25bcdb5b5cb58594f352e5c4cad713580e
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-06-27 22:57:20 +0300

    Updated to EfCore 7.0.X

--- src/EntityFrameworkCore/BIT.Data.Sync.EfCore.Pomelo.MySql/PomeloMySqlDeltaGenerator.cs
+++ src/EntityFrameworkCore/BIT.Data.Sync.EfCore.Pomelo.MySql/PomeloMySqlDeltaGenerator.cs
@@ -1,9 +1,12 @@
 ﻿
 using BIT.EfCore.Sync;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal;
 //using Microsoft.EntityFrameworkCore.Sqlite.Update.Internal;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Update;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Storage.Internal;
 using Pomelo.EntityFrameworkCore.MySql.Update.Internal;
@@ -13,11 +16,26 @@ namespace BIT.Data.Sync.EfCore.Sqlite
     public class PomeloMySqlDeltaGenerator : DeltaGeneratorBase
     {
 
-        public PomeloMySqlDeltaGenerator()
-        {
+       
 
+        public PomeloMySqlDeltaGenerator(MySqlServerVersion serverVersion)
+        {
+            this.serverVersion = serverVersion;
         }
 
+        private static MySqlOptions GetOptions(MySqlServerVersion serverVersion)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            var extension = new MySqlOptionsExtension()
+                .WithServerVersion(serverVersion);
+
+            ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+
+            var options = new MySqlOptions();
+            options.Initialize(optionsBuilder.Options);
+            return options;
+        }
+        MySqlServerVersion serverVersion;
         public override IUpdateSqlGenerator CreateInstance(IServiceProvider serviceProvider)
         {
             TypeMappingSourceDependencies TypeMappingSourceDependencies = serviceProvider.GetService(typeof(TypeMappingSourceDependencies)) as TypeMappingSourceDependencies;
@@ -27,12 +45,14 @@ namespace BIT.Data.Sync.EfCore.Sqlite
             RelationalSqlGenerationHelperDependencies dependencies = new RelationalSqlGenerationHelperDependencies();
             MySqlSqlGenerationHelper sqlGenerationHelper = new MySqlSqlGenerationHelper(dependencies, optionsTypeMapping);
             MySqlTypeMappingSource typeMappingSource = new MySqlTypeMappingSource(TypeMappingSourceDependencies, RelationalTypeMappingSourceDependencies, optionsTypeMapping);
-           
-            var mySqlUpdateSqlGenerator = new MySqlUpdateSqlGenerator(new UpdateSqlGeneratorDependencies(sqlGenerationHelper, typeMappingSource));
+            var optionsUpdateSqlGenerator = new MySqlOptions();
+            serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
+            optionsUpdateSqlGenerator = GetOptions(serverVersion);
+            var mySqlUpdateSqlGenerator = new MySqlUpdateSqlGenerator(new UpdateSqlGeneratorDependencies(sqlGenerationHelper, typeMappingSource), optionsUpdateSqlGenerator);
 
 
 
-            return mySqlUpdateSqlGenerator ;
+            return mySqlUpdateSqlGenerator;
         }
     }
 }
