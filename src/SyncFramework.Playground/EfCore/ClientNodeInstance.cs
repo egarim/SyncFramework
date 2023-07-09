7bcbe065330b38f4c9792c033dcb7c8f9017345c
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-07-09 21:25:49 +0300

    before mud blazor

--- src/SyncFramework.Playground/EfCore/ClientNodeInstance.cs
+++ src/SyncFramework.Playground/EfCore/ClientNodeInstance.cs
@@ -4,5 +4,21 @@
     {
         public string Id { get; set; }
         public BlogsDbContext DbContext { get; set; }
+   
+        public int DeltaCount {
+            get
+            {
+                var CountTask = DbContext.DeltaStore.GetDeltaCountAsync("", this.Id,default);
+                CountTask.Wait();
+                return CountTask.Result;
+            }
+          
+        
+        }
+        public async Task AddBlog(string BlogName)
+        {
+            DbContext.Blogs.Add(new Blog { Name= BlogName });
+            await DbContext.SaveChangesAsync();
+        }
     }
 }
--- src/SyncFramework.Playground/Pages/EfCore.razor.cs
+++ src/SyncFramework.Playground/Pages/EfCore.razor.cs
@@ -7,27 +7,37 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SyncFramework.Playground.EfCore;
 using SyncFramework.Playground.Shared;
+using System;
 using System.Net.Http;
 using System.Security.Principal;
 namespace SyncFramework.Playground.Pages
 {
     public partial class EfCore
     {
-
+        public IQueryable<ClientNodeInstance> ClientNodes
+        {
+            get
+            {
+                return clientNodes.AsQueryable();
+            }
+           
+        }
         [Inject]
         public DeltaGeneratorBase[] deltaGeneratorBases { get; set; }
-        public IDeltaStore deltaStore { get; set; }
+        public IDeltaStore ServerDeltaStore { get; set; }
         public string NodeId { get; set; }
         protected override void OnInitialized()
         {
             base.OnInitialized();
-            deltaStore = new MemoryDeltaStore();
+            ServerDeltaStore = new MemoryDeltaStore();
             NodeId = "MainServer";
         }
         SyncServerComponent serverComponent;
+        private List<ClientNodeInstance> clientNodes = new List<ClientNodeInstance>();
+
         private async void AddClientNode()
         {
-            string DbName = "Database";
+            string DbName = Guid.NewGuid().ToString();
             ServiceCollection serviceCollection = new ServiceCollection();
             serviceCollection.AddSyncFrameworkForSQLite($"Data Source={DbName}_Deltas.db", this.serverComponent.HttpClient, this.serverComponent.NodeId, "Node A", deltaGeneratorBases);
 
@@ -55,12 +65,29 @@ namespace SyncFramework.Playground.Pages
             DbContextInstance.Add(SqliteBlog);
             DbContextInstance.Add(NpgsqlBlog);
             DbContextInstance.Add(PomeloMySqlBlog);
+
+            this.clientNodes.Add(new ClientNodeInstance { Id = DbName, DbContext = DbContextInstance });
             await DbContextInstance.SaveChangesAsync();
-            await DbContextInstance.PushAsync();
+            //await DbContextInstance.PushAsync();
         }
         private static Blog GetBlog(string Name, string Title1, string Title2)
         {
             return new Blog { Name = Name, Posts = { new Post { Title = Title1 }, new Post { Title = Title2 } } };
+        }
+        async Task Delete(Blog b,BlogsDbContext d)
+        {
+            d.Remove(b);
+            await d.SaveChangesAsync();
+           
+        }
+        void Push(ClientNodeInstance p)
+        {
+           
+        }
+
+        void Pull(ClientNodeInstance p)
+        {
+          
         }
     }
 }
