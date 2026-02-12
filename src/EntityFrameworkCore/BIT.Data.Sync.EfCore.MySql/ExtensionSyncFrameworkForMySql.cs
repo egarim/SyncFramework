042fe3213a931239d30612983cd9176fc01f03da
Jose Ojeda <joche.ojeda@bitframeworks.com>
2026-02-12 09:28:55 +0300

    Add MySQL support for SyncFramework
    - Created BIT.Data.Sync.EfCore.MySql project with necessary configurations for NuGet package generation.
    - Implemented ExtensionSyncFrameworkForMySql class to facilitate the integration of SyncFramework with MySQL.
    - Added LICENSE.txt file with "No Liability" Open Source License based on Apache 2.0.

---
+++ src/EntityFrameworkCore/BIT.Data.Sync.EfCore.MySql/ExtensionSyncFrameworkForMySql.cs
@@ -0,0 +1,28 @@
+using BIT.EfCore.Sync;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ExtensionSyncFrameworkForMySql
+    {
+        public static IServiceCollection AddSyncFrameworkForMySql(this IServiceCollection serviceCollection, string mySqlDeltaStoreConnectionString, HttpClient httpClient, string ServerNodeId, string Identity, params DeltaGeneratorBase[] AdditionalDeltaGenerators)
+        {
+            serviceCollection.AddEfSynchronization((options) => 
+            { 
+                options.UseMySQL(mySqlDeltaStoreConnectionString);
+            },
+            httpClient,
+            ServerNodeId, 
+            Identity,
+            AdditionalDeltaGenerators);
+            return serviceCollection;
+        }
+    }
+}
